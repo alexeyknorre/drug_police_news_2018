993933c40a2da15bcb7afcfc26cbb380a156a161
Anastasia Kosacheva <anastasia.kosacheva@icloud.com>
2018-07-02 17:34:07 +0300

    Первые два пункта
    Перекодировка санкций и очистка названий изданий

--- code/0_prepare_data.R
+++ code/0_prepare_data.R
@@ -75,6 +75,23 @@ publishers$publisher[str_detect(publishers$publisher, "Вечерний Став
 publishers$publisher[str_detect(publishers$publisher, "Йошкар-Ола")] <- "Йошкар-Ола"
 publishers$publisher[str_detect(publishers$publisher, "Невское время")] <- "Невское время"
 publishers$publisher[str_detect(publishers$publisher, "ИНТЕР|Интер")] <- "Интер"
+publishers$publisher[str_detect(publishers$publisher, "Слобода")] <- "Слобода"
+publishers$publisher[str_detect(publishers$publisher, "Якутск вечерний|Якутск Вечерний")] <- "Якутск вечерний"
+publishers$publisher[str_detect(publishers$publisher, "Трибуна")] <- "Трибуна"
+publishers$publisher[str_detect(publishers$publisher, "Советская Молодежь|Советская молодежь")] <- "Советская молодежь"
+publishers$publisher[str_detect(publishers$publisher, "Новая новгородская газета")] <- "Новая новгородская газета"
+publishers$publisher[str_detect(publishers$publisher, "Нижегородские новости")] <- "Нижегородские новости"
+publishers$publisher[str_detect(publishers$publisher, "Наше Время|Наше время")] <- "Наше время"
+publishers$publisher[str_detect(publishers$publisher, "Молодой Коммунар|Молодой коммунар")] <- "Молодой коммунар"
+publishers$publisher[str_detect(publishers$publisher, "Красный Север|Красный север")] <- "Красный север"
+publishers$publisher[str_detect(publishers$publisher, "Краснодарские известия")] <- "Краснодарские известия"
+publishers$publisher[str_detect(publishers$publisher, "Кабардино-Балкарская правда|Кабардино-балкарская правда")] <- "Кабардино-Балкарская правда"
+publishers$publisher[str_detect(publishers$publisher, "Аресеньевские вести")] <- "Арсеньевские вести"
+publishers$publisher[str_detect(publishers$publisher, "Вечерняя Казань")] <- "Вечерняя Казань"
+publishers$publisher[str_detect(publishers$publisher, "Вятский наблюдатель")] <- "Вятский наблюдатель"
+publishers$publisher[publishers$publisher == "Известия "] <- "Известия"
+publishers$publisher[publishers$publisher == "Комсомольская правда "] <- "Комсомольская правда"
+publishers$publisher[publishers$publisher == "Арсеньевские вести "] <- "Арсеньевские вести"
 
 ### ###
 # Check publishers_count to see what should be cleaned
@@ -83,6 +100,24 @@ publishers_count <- publishers %>%
   group_by(publisher) %>% 
   summarise(count = sum(count)) %>% 
   arrange(desc(count))
+
+### Sanctions dummies
+
+df$sanction[285] <- "уголовное дело"
+df$sanction[286] <- "уголовное дело"
+df$is_fired <- 0
+df$is_wanted <- 0
+df$is_case <- 0
+df$is_seizure <- 0
+df$is_justification <- 0
+df$is_house_arrest <- 0
+df$is_fired[str_detect(df$sanction, "увол|отстр")] <- 1
+df$is_wanted[str_detect(df$sanction, "розыск")] <- 1
+df$is_case[str_detect(df$sanction, "угол")] <- 1
+df$is_seizure[str_detect(df$sanction, "пров|обыск")] <- 1
+df$is_justification[str_detect(df$sanction, "оправд")] <- 1
+df$is_house_arrest[str_detect(df$sanction, "подп")] <- 1
+df$is_detention[str_detect(df$sanction, "страж|задер|арест")] <- 1
 
 ### Articles from Criminal Code ####
 # TODO
